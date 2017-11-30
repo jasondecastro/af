5548c54d576fb7a0aaeb29ca92e0c5a94f7f14ea
Jason Decastro <jasondecastro@Jasons-MacBook-Pro.local>
2017-11-30 01:07:26 -0500

    push each student instance into its own array for future manipulation

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
 require 'octokit'
+require_relative 'students'
+require 'pry'
+
+students = []
 
 Octokit.configure do |c|
   c.login = 'flatnog'
   c.password = 'flatnog123'
 end
 
-Octokit.user
+Students.students.each do |n|
+	students << Octokit.user("#{n}")
+end
+
+binding.pry
